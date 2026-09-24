b698f135645d6f17749972d013ff0a9d6ee55938
Seenuvasan G <seenuvazzan046@gmail.com>
2026-09-24 08:32:32 +0530

    Time: 1 ms (99.61%) | Memory: 43.1 MB (15.59%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.poll(); 
+            int x = maxHeap.poll(); 
+
+            if (y != x) {
+                maxHeap.offer(y - x);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+    }
+}
